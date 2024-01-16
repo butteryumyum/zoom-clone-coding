6c674f4f94a914f358987de9debc6e8b44f61bc1
butter <kmj52781@naver.com>
2024-01-16 21:45:03 +0900

    Socketio 적용

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import  express  from "express";
+import SocketIO from "socket.io";
 import http from "http";
-import {WebSocketServer} from "ws";
+
 
 const app = express();
 
@@ -13,33 +14,34 @@ app.get("/*", (_, res) => res.redirect("/"));
 const handleListen = () => console.log('Listening on http://localhost:3000');
 
 //http 서버
-const server = http.createServer(app); //requestListener 경로를 지정해야함. 그게 위에서 만든 app이 됨 
-// webSocket 서버
-const wss = new WebSocketServer({ server }); //서버를 전달(pass)해서 서버 두개를 동시에 구동
+const httpServer = http.createServer(app); //requestListener 경로를 지정해야함. 그게 위에서 만든 app이 됨 
+//socketio 서버
+const wsServer = SocketIO(httpServer);
 
-function OnSocketClose() {
-    console.log("Disconnected from Server ❌")
-}
+wsServer.on("connection", socket => {
+    console.log(socket); //backend에서 connection을 받음
+})
 
-const sockets = []; //누군가 연결하면 connection을 이 DB?(array)에 넣을꺼임
+
+/*const sockets = []; //누군가 연결하면 connection을 이 DB?(array)에 넣을꺼임
 
 wss.on("connection", (socket) => { //connection 이 생겼을 때, socket으로 메시지를 보냄
     socket["nickname"] = "Anon"; //socket에 nickname(익명)을 줌
     sockets.push(socket);
     console.log("Connected to Browser ✅")
     socket.on("close", OnSocketClose); //브라우저가 닫혔을 때 log를 띄움
-    socket.on('message', (msg) => {
+    socket.on('message', (msg) => { //브라우저에서 보낸 메시지 도착
         const message = JSON.parse(msg); 
         switch (message.type) {
             case "new_message"://type이 메시지일 경우 화면에 표시
                 sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));//닉네임 property를 socket object에 저장
-            case "nickname": //type이 닉네임일 경우 닉네임을 socket에 넣어줌
+            break;
+                case "nickname": //type이 닉네임일 경우 닉네임을 socket에 넣어줌
                 socket["nickname"] = message.payload
+            break;
         }
     });
-        
-    
-});
+});*/
  
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
 
