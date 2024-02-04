42a216a983a37c0b2afffbc416f871cdebd46fac
butter <kmj52781@naver.com>
2024-02-04 22:21:19 +0900

    Room Notifications

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,15 +4,36 @@ const welcome = document.getElementById("welcome");
 const form = welcome.querySelector("form");
 const room = document.getElementById("room");
 
+
 room.hidden = true; //방 내부 숨기기
 
 let roomName; // 방 이름
+
+function addMessage(message) {
+    const ul = room.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerText = message;
+    ul.appendChild(li);
+}
+
+function handleMessageSubmit(event) {
+    event.preventDefault();
+    const input = room.querySelector("input");
+    const value = input.value;
+    socket.emit("new_message", input.value, roomName, ()=> {//backend로 첫번째 argument인 input.value와 함께 new_message event를 보냄
+        addMessage(`You:${value}`); //그 뒤에 호출될 function
+    });
+    input.value = "";
+        
+}
 
 function ShowRoom() {
     welcome.hidden = true; //방이름 입력 숨기기
     room.hidden = false; // 방 내부 보이기
     const h3 = room.querySelector("h3"); //h3 태그 가져오기
     h3.innerText = `Room ${roomName}`; //방 이름을 입력받은 값으로 변경
+    const form = room.querySelector("form"); //welcome을 숨기고 room을 보여준 showRoom에서 form을 찾음
+    form.addEventListener("submit", handleMessageSubmit);
 }
 
 function handleRoomSubmit(event) {
@@ -24,3 +45,13 @@ function handleRoomSubmit(event) {
 }
 
 form.addEventListener("submit", handleRoomSubmit); 
+
+
+socket.on("welcome", () => {
+    addMessage("someone joined!");
+});
+
+socket.on("bye", () => {
+    addMessage("someone left ㅠㅠ");
+});
+socket.on("new_message", addMessage)
--- src/server.js
+++ src/server.js
@@ -24,10 +24,17 @@ wsServer.on("connection", socket => {
     });
    socket.on("enter_room", (roomName, done) => { //frontend에서 받은 done에 해당하는 ShowRoom 값을 받음
     socket.join(roomName);
-    done();
-    
-    
+    done(); //fornt에 있는 showRoom()을 실행함
+    socket.to(roomName).emit("welcome"); //event를 참가한 방의 사람들에게 emit해줌
    }); 
+   socket.on("disconnecting", () => {
+    socket.rooms.forEach(room => socket.to(room).emit("bye")); //클라이언트와 서버가 연결이 끊어지기 전에 bye event를 emit
+   });
+   socket.on("new_message", (msg, room, done) => {
+    socket.to(room).emit("new_message", msg); //payload는 방금 받은 메시지(msg를 의미)
+    done(); //done function 호출
+   })
+
 });
 
 
